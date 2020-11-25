441e171dbb35b0f5097870334e9f367885eccca5
keyoung0322 <keyoung0322@gmail.com>
2020-11-25 17:14:12 +0900

    Tap modified

--- LedThread.java
+++ LedThread.java
@@ -2,38 +2,38 @@ import java.io.*;
 import java.net.*;
 
 public class LedThread extends RPI_cont implements Runnable {
-	private Socket clientSock;
-	BufferedReader buffer;
+    private Socket clientSock;
+    BufferedReader buffer;
 
-	public LedThread(Socket socket) {
-		this.clientSock = socket;
+    public LedThread(Socket socket) {
+	this.clientSock = socket;
 
-		try {
-			buffer = new BufferedReader(new InputStreamReader((clientSock.getInputStream())));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	try {
+	    buffer = new BufferedReader(new InputStreamReader((clientSock.getInputStream())));
+	} catch (IOException e) {
+	    e.printStackTrace();
 	}
+    }
 
-	public void run() {
-		//System.out.println("thread start!");
-		while(true) {
-			String msg = listen();
-			if(msg.compareTo("LedOff") == 0) {
-				RPI_cont.led.low();
-				System.out.println("ledoff");
-			}
-		}
+    public void run() {
+	//System.out.println("thread start!");
+	while(true) {
+	    String msg = listen();
+	    if(msg.compareTo("LedOff") == 0) {
+		RPI_cont.led.low();
+		System.out.println("ledoff");
+	    }
 	}
+    }
 
-	public String listen() {
-		String msg="";
-		try {
-			msg= buffer.readLine();
-			//System.out.println("msg:"+msg);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return msg;
+    public String listen() {
+	String msg="";
+	try {
+	    msg= buffer.readLine();
+	    //System.out.println("msg:"+msg);
+	} catch (IOException e) {
+	    e.printStackTrace();
 	}
+	return msg;
+    }
 }
--- Server.java
+++ Server.java
@@ -2,26 +2,26 @@ import java.io.IOException;
 import java.net.*;
 
 public class Server extends RPI_cont{
-	static int port = 8888;
-	static ServerSocket server;
+    static int port = 8888;
+    static ServerSocket server;
 
-    	public static void main(String[] args) throws IOException {
-		try {
-                        server = new ServerSocket(port);
+    public static void main(String[] args) throws IOException {
+	try {
+	    server = new ServerSocket(port);
 
-                        while(true){
-								Socket client = server.accept();
-                                InetAddress inet = client.getInetAddress();
-                                String ip = inet.getHostAddress();
-                                System.out.println(ip+"-�젒�냽�옄 諛쒓껄");
+	    while(true){
+		Socket client = server.accept();
+		InetAddress inet = client.getInetAddress();
+		String ip = inet.getHostAddress();
+		System.out.println(ip+"connect!");
 
-                                //���솕�슜 �벐�젅�뱶 �깮�꽦 諛� �냼耳�
-                                Thread thread = new Thread(new LedThread(client));
-                                System.out.println("start");
-                                thread.start();
-                        }
-                } catch(IOException e) {
-                        e.printStackTrace();
-                }
+		/* create led thread */
+		Thread thread = new Thread(new LedThread(client));
+		System.out.println("start");
+		thread.start();
+	    }
+	} catch(IOException e) {
+	    e.printStackTrace();
 	}
+    }
 }
